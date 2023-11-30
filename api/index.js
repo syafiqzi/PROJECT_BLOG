35d693bb2100f834b83cc7a513f6156f01591618
piji <syafiqzi98@gmail.com>
2023-12-01 00:50:50 +0800

    Auth

--- api/index.js
+++ api/index.js
@@ -2,9 +2,15 @@ const express = require('express');
 const cors = require('cors');
 const { default: mongoose } = require('mongoose');
 const User = require('./models/User');
+const bcrypt = require('bcryptjs');
 const app = express();
+const jwt = require ('jsonwebtoken');
 
-app.use(cors());
+
+const salt = bcrypt.genSaltSync(10); 
+const secret = 'adfnrflsiefje'; 
+
+app.use(cors({credentials:true, origin:'http://localhost:3000'}));
 app.use(express.json());
 
 mongoose.connect('mongodb+srv://syafiqzi98:sJRtpGU3wbVwTVXy@cluster0.cw6eujq.mongodb.net')
@@ -14,13 +20,32 @@ mongoose.connect('mongodb+srv://syafiqzi98:sJRtpGU3wbVwTVXy@cluster0.cw6eujq.mon
 app.post('/Register', async(req, res) => {  
   const {username, password} = req.body;
   try{
-    const userDoc = await User.create ({username, password});
+    const userDoc = await User.create ({
+      username, 
+      password:bcrypt.hashSync(password,salt)});
     res.json(userDoc);
   }catch(e){
      res.status(400).json(e); 
+  } 
+}); 
+
+app.post('/login', async(req, res) => {  
+  const {username, password} = req.body;
+  const userDoc = await User.findOne({username});
+  const passOk = bcrypt.compareSync(password, userDoc.password);
+ 
+  if (passOk){
+    //login
+    jwt.sign({username,id:userDoc._id}, secret,{}, (err, token)=> {
+      if (err) throw err;
+      res.cookies('token', token).json('ok');
+    });
+  } else {
+    //not login
+    res.status(400).json('Wrong credential')
   }
-  
-  // res.json({requestData: {username,password}});
+
+
 }); 
 
 // var { Client } = require('pg')
